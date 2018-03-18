0efbde9053803bd455130d09c4fa75217a128718
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2018-03-18 14:15:40 +0300

    Add using environments for mongodb and mysql uri

--- src/config/index.js
+++ src/config/index.js
@@ -1,24 +1,17 @@
 import mongoose from 'mongoose';
 import session from 'express-session';
 import connectMongo from 'connect-mongo';
-import merge from 'lodash/merge';
-import fs from 'fs';
-import path from 'path';
 
 import defaultConfig from './config.json';
 
 const MongoStore = connectMongo(session);
-const secretPath = process.env.DBTRY_SECRET || path.join(__dirname, 'secret.json');
-let secret = null;
 
-try {
-	secret = JSON.parse(fs.readFileSync(secretPath, 'utf8'));
-} catch (err) {
-	console.error(`Error: ${err.message}. Used default configs as secret`);
-}
-
-const config = secret ? merge(defaultConfig, secret) : defaultConfig;
+const config = defaultConfig;
 config.port = process.env.PORT || config.port;
+
+config.mongodb.url = process.env.MONGODB_URI;
+config.mysql.url = process.env.CLEARDB_DATABASE_URL;
+
 config.session.store = new MongoStore({ mongooseConnection: mongoose.connection });
 
 export default config;
